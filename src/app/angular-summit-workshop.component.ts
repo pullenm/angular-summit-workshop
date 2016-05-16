2e0c736bd51d92dc78cd2ed2ccd547e52a435807
rg42775 <PullenMatthewR@JohnDeere.com>
2016-05-16 10:38:20 -0500

    Making it look better

--- src/app/angular-summit-workshop.component.ts
+++ src/app/angular-summit-workshop.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './+home';
+import { Routes , ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
+import { TeamComponent } from './+team';
+import { DetailComponent } from './+detail';
+
+
 
 @Component({
   moduleId: module.id,
   selector: 'angular-summit-workshop-app',
   templateUrl: 'angular-summit-workshop.component.html',
-  styleUrls: ['angular-summit-workshop.component.css']
+  styleUrls: ['angular-summit-workshop.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS]
 })
+@Routes([
+  {path: '/', component: HomeComponent},
+  {path: '/home', component: HomeComponent},
+  {path: '/team', component: TeamComponent},
+  {path: '/detail/:id', component: DetailComponent}
+])
 export class AngularSummitWorkshopAppComponent {
   title = 'angular-summit-workshop works!';
 }
